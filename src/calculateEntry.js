905fae620d7e30be59898317b6d6bbae9fd06244
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-31 15:52:23 -0300

    1. Implemente a função getSpeciesByIds.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -9,4 +9,4 @@ function calculateEntry(entrants) {
 }
 
 module.exports = { calculateEntry, countEntrants };
-//xablau
+
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => {
+  const getSpeciesById = species.filter((specie) => ids.includes(specie.id));
+  return getSpeciesById;
+};
 
 module.exports = getSpeciesByIds;
