0f66717e9f328dc76ac0baa8f4551d193cc5c120
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-02 11:16:07 -0300

    2 - Implemente a função getAnimalsOlderThan que verifica se todos os animais de uma espécie possuem a idade mínima especificada

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = species.filter((especie) => especie.name === animal)[0].residents
+    .every((idade) => idade.age >= age);
+
+  return animals;
 }
 
 module.exports = getAnimalsOlderThan;
