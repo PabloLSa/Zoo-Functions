fcbae66a9ff998b733e3d852d5da4177f4882ca5
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-02 18:08:01 -0300

    4. Implemente a função getRelatedEmployees.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
-const {employees} = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  //
+  const manager = employees.some((employee) => employee.managers.includes(id));
+  return manager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  return employees.filter((employee) => employee.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
+
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = { isManager, getRelatedEmployees };
