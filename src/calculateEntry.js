57e0402478ef9a12d9d3d9b9be2727827cad6f55
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-03 22:13:10 -0300

    8. Implemente a função calculateEntry.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const child = entrants.filter((element) => element.age < 18).length;
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50).length;
+
+  const senior = entrants.filter((element) => element.age >= 50).length;
+
+  return { child, adult, senior };
+};
+
+const calculateEntry = (entrants) => {
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+
+  const soma = countEntrants(entrants).adult * prices.adult + countEntrants(entrants)
+    .child * prices.child + countEntrants(entrants).senior * prices.senior;
+  return soma;
+};
 
 module.exports = { calculateEntry, countEntrants };
