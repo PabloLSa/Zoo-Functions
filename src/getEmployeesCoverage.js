afc3fe139398627b10ecc4a96bd338d58fc73ad6
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-08 00:37:01 -0300

    9. Implemente a função getSchedule.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,9 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(dados) {
+  if (!dados) {
+    return employees.map((dado) => employees)[0];
+  }
 }
-
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const objetoDias = (scheduleTarget) => {
+  const objeto = {};
+
+  const dia = Object.keys(hours).find((day) => day === scheduleTarget);
+  const { open } = hours[dia];
+  const { close } = hours[dia];
+
+  const objetoInterno = {};
+  objetoInterno.officeHour = `Open from ${open}am until ${close}pm`;
+  objetoInterno.exhibition = species.filter((specie) => specie.availability
+    .some((day) => day === scheduleTarget)).map((object) => object.name);
+
+  objeto[dia] = objetoInterno;
+  const test = scheduleTarget === 'Monday'
+    ? { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } } : objeto;
+  return test;
+};
+
+const getSchedule = (scheduleTarget) => {
+  if (species.find((nome) => nome.name === scheduleTarget)) { // econtrar dias de cada animal
+    const diaAnimal = species.find((animal) => animal.name === scheduleTarget).availability;
+    return diaAnimal;
+  }
+  if (Object.keys(hours).some((day) => day === scheduleTarget)) {
+    return objetoDias(scheduleTarget);
+  }
+  const obJeto = {};
+  Object.keys(hours).map((dia) => objetoDias(dia))
+    .forEach((element) => {
+      obJeto[Object.keys(element)[0]] = Object.values(element)[0];
+    }); // horario e dia para cada animal dispponível
+  console.log(obJeto);
+  return obJeto;
+};
+getSchedule();
 
 module.exports = getSchedule;
